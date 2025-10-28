89c92520a44342861fe15a8a61a708d08033c04f
Maddax10 <maximilien01993@gmail.com>
2025-10-28 03:13:50 +0100

    feat: enhance ProjectC component layout and styling, improve input handling and add image section

--- src/components/ProjectC.vue
+++ src/components/ProjectC.vue
@@ -16,30 +16,34 @@
     <div class="projectCard">
         <div class="projectCard__titleCard">{{ props.project.title.toString() }}</div>
         <div class="projectCard__infos">
-            <div class="projectCard__titleSection projectCard__section">
+            <div class="projectCard__section">
                 <div class="projectCard__title">Titre</div>
                 <input class="projectCard__input" type="text" :value=props.project.title.toString() />
             </div>
-            <div class="projectCard__description projectCard__section">
+            <div class="projectCard__section">
                 <div class="projectCard__title">Description</div>
-                <input class="projectCard__input" type="text" :value=props.project.description.toString() />
+                <textarea class="projectCard__textarea" :value=props.project.description.toString() />
             </div>
-            <div class="projectCard__githubLink projectCard__section">
+            <div class="projectCard__section">
                 <div class="projectCard__title">Lien github</div>
                 <input class="projectCard__input" type="text" :value=props.project.github.toString() />
             </div>
-            <div class="projectCard__imageSection projectCard__section">
-                <div class="projectCard__title">Lien de l'image</div>
-                <input class="projectCard__input" type="text" :value=props.project.image_path.toString() />
-                <img class="projectCard__image" :src=props.project.image_path.toString() />
-            </div>
-            <div class="projectCard__skills projectCard__section">
+            <div class="projectCard__section">
                 <div class="projectCard__title">Skills</div>
                 <div class="projectCard__skillsList">
                     <img class="projectCard__skill" v-for="(skill, key) in props.project.skills" :key="key"
                         :src=skill.image_path.toString() :alt=skill.name.toString()>
                 </div>
             </div>
+            <div class="projectCard__section">
+                <div class="projectCard__title">Lien de l'image</div>
+                <input class="projectCard__input" type="text" :value=props.project.image_path.toString() />
+
+            </div>
+
+        </div>
+        <div class="projectCard__imageSection">
+            <img class="projectCard__image" :src=props.project.image_path.toString() />
         </div>
         <button class="projectCard__sendButton">Modifier le projet</button>
     </div>
